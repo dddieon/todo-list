e948de85401f98b9505e385bb7f9ba22c016b9ba
DDDieon <59174247+DDDieon@users.noreply.github.com>
2020-10-13 21:19:54 +0900

    Set Remove Button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState, useRef } from 'react'
 import TodoTemplate from './components/TodoTemplate'
-import './App.css'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
 
@@ -22,11 +21,34 @@ function App() {
             checked: false,
         },
     ])
+    // useState 대신 useRef로 id를 담자
+    const nextId = useRef(4)
+    // 변수 남는 함수
+    const onInsert = useCallback(
+        (text) => {
+            const todo = {
+                id: nextId.current,
+                text,
+                // onInsert(value)를 하면 input의 값이 text속성으로 들어온다
+                checked: false,
+            }
+            setTodos(todos.concat(todo))
+            nextId.current += 1
+            // ref는 1씩 올라간다
+        },
+        [todos],
+    )
+    const onRemove = useCallback(
+        (id) => {
+            setTodos(todos.filter((todo) => todo.id !== id))
+        },
+        [todos],
+    )
     return (
         <>
             <TodoTemplate>
-                <TodoInsert />
-                <TodoList todos={todos} />
+                <TodoInsert onInsert={onInsert} />
+                <TodoList todos={todos} onRemove={onRemove} />
             </TodoTemplate>
         </>
     )
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,15 +7,15 @@ import {
 import './TodoListItem.scss'
 import cn from 'classnames'
 
-export default function TodoListItem({ todo }) {
-    const { text, checked } = todo
+export default function TodoListItem({ todo, onRemove }) {
+    const { id, text, checked } = todo
     return (
         <div className="TodoListItem">
             <div className={cn('checkbox', { checked })}>
                 <MdCheckBoxOutlineBlank />
                 <div className="text">{todo.text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline />
             </div>
         </div>
